87e3fc9341e776e0dfab053dd2c40c1c2851c482
Anna Paruryan <a.sahradyan72@gmail.com>
2024-08-17 15:34:37 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const lazyLoadInstance = new LazyLoad({
+//         elements_selector: ".lazy",
+//         debug: true
+//     });
+//
+//     function lazyLoadBackground() {
+//         const headers = document.querySelectorAll('.header[data-bg]');
+//         headers.forEach(header => {
+//             const bgImage = header.getAttribute('data-bg');
+//             header.style.backgroundImage = `url(${bgImage})`;
+//         });
+//     }
+//
+//     lazyLoadBackground();
+//
+//     console.log(lazyLoadInstance);
+// });
+document.addEventListener('DOMContentLoaded', function() {
+    const lazyLoadInstance = new LazyLoad({
+        elements_selector: ".lazy",
+        debug: true
+    });
+
+    function lazyLoadBackground() {
+        // Set background images for .header elements
+        const headers = document.querySelectorAll('.header[data-bg]');
+        headers.forEach(header => {
+            const bgImage = header.getAttribute('data-bg');
+            if (bgImage) {
+                header.style.backgroundImage = `url(${bgImage})`;
+            }
+        });
+        const tops = document.querySelectorAll('.efficiency__top[data-bg]');
+        tops.forEach(top => {
+            const bgImage = top.getAttribute('data-bg');
+            if (bgImage) {
+               top.style.backgroundImage = `url(${bgImage})`;
+            }
+        });
+    }
+
+    lazyLoadBackground();
+
+    console.log(lazyLoadInstance);
+});
